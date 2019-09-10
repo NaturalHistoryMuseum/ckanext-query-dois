bfeca239e1cadd86dcebcbe76d6c60b0c5989f5f
Josh Humphries <j.humphries@nhm.ac.uk>
2019-09-10 11:54:59 +0100

    Fix QueryDOI.on_resource function
    This function was broken and although it didn't error, it never matched
    any resources when used. Now that the JSONB column type is available
    (thanks to a bump in sqlalchemy version from the ckan upgrade) we can
    switch the column type to JSONB and use the has_key function create a
    filter.

--- ckanext/query_dois/model.py
+++ ckanext/query_dois/model.py
@@ -9,39 +9,7 @@ import json
 from sqlalchemy import Column, UnicodeText, DateTime, Table, BigInteger, types
 
 from ckan.model import meta, DomainObject
-
-
-class JsonType(types.UserDefinedType):
-    '''
-    A JSONB column type for PostgreSQL databases. This should be removed and replaced by an
-    sqlalchemy core implementation when one becomes available in newer versions of CKAN.
-    '''
-
-    def get_col_spec(self):
-        return u'jsonb'
-
-    def is_mutable(self):
-        return True
-
-    def python_type(self):
-        return dict
-
-    def bind_processor(self, dialect):
-        def process(value):
-            if value is not None:
-                return json.dumps(value)
-            else:
-                return None
-        return process
-
-    def result_processor(self, dialect, coltype):
-        def process(value):
-            if value is None:
-                return None
-            else:
-                return value
-
-        return process
+from sqlalchemy.dialects.postgresql import JSONB
 
 
 query_doi_table = Table(
@@ -52,11 +20,11 @@ query_doi_table = Table(
     Column(u'doi', UnicodeText, nullable=False, index=True, unique=True),
     # json column representing the resources in this query and their rounded versions, it is a
     # straight map from resource_id: version
-    Column(u'resources_and_versions', JsonType, nullable=False),
+    Column(u'resources_and_versions', JSONB, nullable=False),
     # the timestamp when the doi was created
     Column(u'timestamp', DateTime, nullable=False),
     # the query dict that produces the data for this doi
-    Column(u'query', JsonType, nullable=False),
+    Column(u'query', JSONB, nullable=False),
     # the hash for the query that produces the data for this doi - this is used in conjunction with
     # the version to check if the query has been run before
     Column(u'query_hash', UnicodeText, nullable=False, index=True),
@@ -97,7 +65,13 @@ class QueryDOI(DomainObject):
 
     @staticmethod
     def on_resource(resource_id):
-        return QueryDOI.resources_and_versions.op(u'->>')(resource_id) != None
+        '''
+        A convenience method to filter by a specific resource id.
+
+        :param resource_id: the resource id
+        :return: an sqlalchemy boolean expression
+        '''
+        return QueryDOI.resources_and_versions.has_key(resource_id)
 
 
 class QueryDOIStat(DomainObject):
